391c8a3a6e1206aca3116340df00a0e36c01e61e
OGZ-Repository <plathur.ler@gmail.com>
2025-09-21 15:23:13 +0200

    add new projects

--- practice.js
+++ practice.js
@@ -1,14 +1,15 @@
-const nums=[1,2,3];
-nums.forEach(n => console.log("Number:", n));
+const nums1=[1,2,3];
+nums1.forEach(n => console.log("Number:", n));
 
-const nums=[1,2,3];
-const doubled=nums.map(n => n * 2);
+const nums2=[1,2,3];
+const doubled=nums2.map(n => n * 2);
 console.log(doubled);
 
-const nums=[1,2,3,4,5];
-const evens=nums.filter(n=> n % 2 === 0);
+const nums3=[1,2,3,4,5];
+const evens=nums3.filter(n=> n % 2 === 0);
 console.log(evens);
 
-const nums=[5,10,15];
-const total=nums.reduce((acc, curr,) => acc + curr, 0);
+const nums4=[5,10,15];
+const total=nums4.reduce((acc, curr) => acc + curr, 0);
 console.log(total);
+3.9
